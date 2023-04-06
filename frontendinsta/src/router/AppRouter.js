5801063f2182468ba9244f05c67f37c61d362434
saurabh rajpoot <saurabh941193@gmail.com>
2023-04-06 10:27:22 +0530

    added newPost list and http fetch methods

--- frontendinsta/src/router/AppRouter.js
+++ frontendinsta/src/router/AppRouter.js
@@ -1,15 +1,16 @@
 import React from "react";
 import {BrowserRouter,Route,Routes} from "react-router-dom";
 import LandingPage from "../components/LandingComponent/landing";
+import NewPost from "../components/post/new/newpost";
 export default function AppRouter(){
     return(
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<LandingPage/>} />
-            {/* <Route path="post" element={<postView />}>
-                <Route path="new" element={<NewPost />} />
-                <Route path="all" element={<AllPost />} />
-            </Route> */}
+            {/* <Route path="post" element={<postView />}> */}
+        <Route path="new" element={<NewPost />} />
+                {/* <Route path="all" element={<NewPost />} /> */}
+            {/* </Route> */}
         </Routes>
         
         </BrowserRouter>
